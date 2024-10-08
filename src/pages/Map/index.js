cc41b12f6abc0aead680773ef71fa492faca49e6
kiu-chan <khanhk66uet@gmail.com>
2024-10-08 11:31:09 +0700

    render map

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,9 +1,33 @@
-import {useEffect, useState} from "react";
+// src/Map.js
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-function Map() {
-    return (
-        <div>test</div>
-    )
-}
+// Đặt icon cho Marker
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+const Map = () => {
+  const position = [10.7769, 106.6950]; // Toạ độ của vị trí bạn muốn hiển thị (TP.HCM)
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: "400px", width: "800px" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          Đây là một marker!
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
 
 export default Map;
